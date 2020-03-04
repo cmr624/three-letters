a72ae92729cbbb345c5e2aa91da218cfd2706f13
Carlos Michael Rodriguez <cmr624@nyu.edu>
2020-03-04 00:28:42 -0500

    adding

---
+++ src/scripts/objects/letterTile.ts
@@ -0,0 +1,57 @@
+import { DraggableSprite, DraggableContainer } from "../cm-phaser-library/src/sprites/interactive/draggableSprite";
+import { defaultText } from "../cm-phaser-library/src/objects/textStyles";
+import { PRELOADED_KEYS } from "../../utils/dist/preloadedKeyObject";
+
+
+export class LetterTile extends DraggableContainer{
+
+    sprite : Phaser.GameObjects.Sprite;
+    text : Phaser.GameObjects.Text;
+    defaultX : number;
+    defaultY : number;
+    inDestination : boolean;
+
+    constructor(scene, x, y, public letter : string){
+        super(scene, x, y);
+        this.sprite = this.scene.add.image(0, 0, PRELOADED_KEYS["TILE-BG-OPEN"].key);
+        this.add(this.sprite);
+        this.setSize(this.sprite.width, this.sprite.height);
+        this.text = this.scene.add.text(0, 0, this.letter, defaultText).setOrigin(.5);
+        this.add(this.text);
+        this.initInteractive();
+        this.setScale(2);
+        this.inDestination = false;
+    }
+
+    dragStart(){
+        super.dragStart();
+        this.sprite.setTexture(PRELOADED_KEYS["TILE-BG-PRESSED"].key);
+    }
+
+    drop(){
+        super.drop();
+        this.sprite.setTexture(PRELOADED_KEYS["TILE-BG-OPEN"].key);
+        if (!this.inDestination) {
+            this.moveToDefaultPosition();
+        }
+    }
+
+    inDestinationX;
+    inDestinationY;
+    dropOnTarget(){
+        this.inDestination = true;
+        this.inDestinationX = this.x;
+        this.inDestinationY = this.y;
+    }
+
+    moveToDefaultPosition(){
+        this.x = this.defaultX;
+        this.y = this.defaultY;
+    }
+
+    setDefaultPosition(x : number, y : number){
+        this.defaultX = x;
+        this.defaultY = y;
+        this.moveToDefaultPosition();
+    }
+}
